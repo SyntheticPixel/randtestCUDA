8007d2acd4d0821044146c2180446d80ae1aebe3
SyntheticPixel <erik.veldeman@gmail.com>
2017-02-15 16:05:03 +0100

    update
    update

--- main.cu
+++ main.cu
@@ -52,27 +52,6 @@ Generate a uniform distribution on a unit disc
 __device__ glm::vec3 RandUniformDisc(curandState *s){
 	glm::vec3 p;
 
-	/*
-	float a = curand_uniform(s);
-	float b = curand_uniform(s);
-
-	//swap if b < a
-	if (b < a){
-		float c;
-		c = a;
-		a = b;
-		b = c;
-	}
-
-	// this boils down to unit radius
-	p.x = a*cos(2 * M_PI * (a / b));
-	p.y = b*sin(2 * M_PI * (a / b));
-	p.z = 0.0f;
-
-	return p;
-	*/
-
-
 	float t = curand_uniform(s) *2 * M_PI;
 	float r = 2 * curand_uniform(s);
 	if(r > 2){r -= 2.0f;}
@@ -122,18 +101,30 @@ __device__ glm::vec3 RandUniformSphere(curandState *s, float radius){
 	return p;
 }
 
-__device__ void sample(glm::vec3 *s, unsigned long id, unsigned int seed){
+__device__ void sample(int option, glm::vec3 *s, unsigned long id, unsigned int seed){
 
 	glm::vec3 t;
 
 	curandState rngState;
 	curand_init(WangHash(seed)+id, 0, 0, &rngState);
 
-	// function to test
-	//t = RandUniformSquare(&rngState);
-	t = RandUniformDisc(&rngState);
-	//t = RandCosineHemisphere(&rngState, glm::vec3(0.0f, 1.0f, 0.0f));
-	//t = RandUniformSphere(&rngState, 1.0f);
+	switch(option){
+	case 0:
+		t = RandUniformSquare(&rngState);
+		break;
+	case 1:
+		t = RandUniformDisc(&rngState);
+		break;
+	case 2:
+		t = RandCosineHemisphere(&rngState, glm::vec3(0.0f, 0.0f, 1.0f));
+		break;
+	case 3:
+		t = RandUniformSphere(&rngState, 1.0f);
+		break;
+	default:
+		t = RandUniformSquare(&rngState);
+		break;
+	}
 
 	// copy back
 	s->x = t.x;
@@ -141,36 +132,40 @@ __device__ void sample(glm::vec3 *s, unsigned long id, unsigned int seed){
 	s->z = t.z;
 }
 
-__global__ void KERNEL_SAMPLE(glm::vec3 *v, int samples, unsigned int seed){
+__global__ void KERNEL_SAMPLE(int option, glm::vec3 *v, int samples, unsigned int seed){
 
 	unsigned long blockId = blockIdx.x + blockIdx.y * gridDim.x;
 	unsigned long threadId = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
-	if (threadId >= samples) return;
+	if (threadId == samples) return;
 
 	// generate sample
-	sample(&v[threadId], threadId, seed);
+	sample(option, &v[threadId], threadId, seed);
 }
 
 // main
-int main(){
+int main(int argc, const char * argv[]){
 	glm::vec3	*host_vectors;
 	glm::vec3	*device_vectors;
 	size_t		size;
+	ofstream	txtfile;
 
-	std::string input="";
 	int x_samples = 1;
 	int y_samples = 1;
+	int option = 0;
 
-	std::cout << "Enter the number of  X samples:" << std::endl;
-	cin >> x_samples;
-	if(x_samples < 1) x_samples = 1;
-
-	std::cout << "Enter the number of  Y samples:" << std::endl;
-	cin >> y_samples;
-	if(y_samples < 1) y_samples = 1;
+	// make sure we have exactly 4 arguments
+	if(argc == 4){
+		x_samples = atoi(argv[1]);
+		y_samples = atoi(argv[2]);
+		option = atoi(argv[3]);
+	}else{
+		cout << " ./randtestCUDA -samplesX -samplesY -option (0=square, 1=disc, 2=hemisphere, 3=sphere)" << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	// calculate memory size
-	size = sizeof(glm::vec3)*x_samples*y_samples;
+	unsigned int total_samples = x_samples*y_samples;
+	size = sizeof(glm::vec3)*total_samples;
 
 	// assign on host
 	cudaHostAlloc((void**)&host_vectors, size, cudaHostAllocWriteCombined);
@@ -181,26 +176,24 @@ int main(){
 	// sample
 	dim3 blockSize = dim3(16, 16, 1);	// 256 threads
 	dim3 gridSize = dim3((x_samples + blockSize.x - 1) / blockSize.x, (y_samples + blockSize.y - 1) / blockSize.y, 1);
-	KERNEL_SAMPLE<<<gridSize, blockSize>>>(device_vectors, x_samples*y_samples, time(NULL));
+	KERNEL_SAMPLE<<<gridSize, blockSize>>>(option, device_vectors, total_samples, clock());
 	cudaDeviceSynchronize();
 
 	// copy from device to host
 	cudaMemcpy(host_vectors, device_vectors, size, cudaMemcpyDeviceToHost);
 
 	// open the file
-	ofstream ascfile;
-	ascfile.open("out.txt");
+	txtfile.open("out.txt");
 
 	// write out the file
-	for(int i=0; i < x_samples*y_samples; i++){
+	for(int i=0; i < total_samples; i++){
 		glm::vec3 v = host_vectors[i];
 		// print to file
-		ascfile << v.x << " " << v.y << " " << v.z << "\n";
-
+		txtfile << v.x << ";" << v.y << ";" << v.z << "\n";
 	}
 
 	// close the file
-	ascfile.close();
+	txtfile.close();
 
 	// delete device memory
 	if( device_vectors != NULL ){
